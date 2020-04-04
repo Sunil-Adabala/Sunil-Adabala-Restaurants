7cb267caebdfc0c15eb8623801dc4c3a0cb2d8ca
Sunil-Adabala <adabalasunil@gmail.com>
2020-04-04 18:33:56 +0530

    add basic user auth

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 //jshint esversion:6
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const passport = require('passport')
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose")
 var Restaurant = require("./models/restaurant")
 var seedDB = require("./seed")
 var Comment = require("./models/comment")
+var User = require("./models/user")
 
 
 seedDB();
@@ -16,12 +23,30 @@ seedDB();
 const app = express();
 mongoose.connect("mongodb://localhost:27017/restaurantsDB");
 
+//use express-session and perform actions
+app.use(require("express-session")({
+  secret: process.env.SESSION_SECRET, //secret to encode or decode the sessions
+  resave: false,
+  saveUninitialized: false
+}))
+
 app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
+
+passport.use(new LocalStrategy(User.authenticate()))
+//reading the data from the session that is encoded and once done then  encode
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function(req,res,next){
+  res.locals.currentUser= req.user;
+  next();
+})
 //
 // var restaurants = [{
 //     name: "paradise",
@@ -52,134 +77,190 @@ app.use(express.static(__dirname+"/public"));
 //     });
 
 
-    //TODO
-    app.get("/", function(req, res) {
-      res.render("landing");
-    });
+//TODO
+app.get("/",isLoggedIn, function(req, res) {
+  res.render("landing");
+});
 
 
 
-    app.get("/restaurants", function(req, res) {
-      //get restaurants from
-      Restaurant.find({},function(err,allrestaurants){
-        if(err){
-          console.log(err)
-        }
-        else{
-          res.render("index", {
-            restaurants: allrestaurants
-          });
-        }
+app.get("/restaurants", function(req, res) {
+  //get restaurants from
+  Restaurant.find({}, function(err, allrestaurants) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render("index", {
+        restaurants: allrestaurants,
+        currentUser:req.user
       });
+    }
+  });
 
 
-    });
+});
 
-        app.post("/restaurants", function(req, res) {
-          var name= req.body.restaurant;
-          var image= req.body.imglink;
-          var description=req.body.desc;
+app.post("/restaurants",isLoggedIn, function(req, res) {
+  var name = req.body.restaurant;
+  var image = req.body.imglink;
+  var description = req.body.desc;
 
-          var newRestaurant={name:name,image:image,description:description}
-          Restaurant.create(newRestaurant,function(err,newlycreated){
-            if(err){
-              console.log(err)
-            }
-            else{
-              console.log(newlycreated);
-              res.redirect("restaurants");
+  var newRestaurant = {
+    name: name,
+    image: image,
+    description: description
+  }
+  Restaurant.create(newRestaurant, function(err, newlycreated) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(newlycreated);
+      res.redirect("restaurants");
 
-            }
-          })
-          // restaurants.push({
-          //   name: req.body.restraunt,
-          //   image: req.body.imglink
-          // });
-          // console.log(restaurants)
-          // res.redirect("restaurants");
+    }
+  })
+  // restaurants.push({
+  //   name: req.body.restraunt,
+  //   image: req.body.imglink
+  // });
+  // console.log(restaurants)
+  // res.redirect("restaurants");
 
-        });
+});
 
-    app.get("/restaurants/new", function(req, res) {
-      res.render("newrestaurant")
-    })
+app.get("/restaurants/new",isLoggedIn, function(req, res) {
+  res.render("newrestaurant")
+})
 
-    app.get("/restaurants/:id",function(req,res){
+app.get("/restaurants/:id", isLoggedIn,function(req, res) {
+  // console.log(req.params.id)
+
+  Restaurant.findById(req.params.id).populate("comments").exec(function(err, foundrestaurant) {
+    if (err) {
+      console.log(err)
+    } else {
       // console.log(req.params.id)
-
-      Restaurant.findById(req.params.id).populate("comments").exec(function(err,foundrestaurant){
-        if(err){
-          console.log(err)
-        }else{
-          // console.log(req.params.id)
-          console.log(foundrestaurant)
-          res.render("show",{restaurant:foundrestaurant});
-        }
+      console.log(foundrestaurant)
+      res.render("show", {
+        restaurant: foundrestaurant
       });
+    }
+  });
 
-      // res.render("show");
-    });
+  // res.render("show");
+});
 
-    app.get("/restaurants/:id/comments/new",function(req,res){
-      Restaurant.findById(req.params.id,function(err,foundrestaurant){
-        if(err){
+app.get("/restaurants/:id/comments/new",isLoggedIn, function(req, res) {
+  Restaurant.findById(req.params.id, function(err, foundrestaurant) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render("newcomment", {
+        restaurant: foundrestaurant
+      });
+    }
+
+  })
+
+})
+// app.get("/restaurants/:id/comments/new",function(req,res){
+//   Restaurant.findById(req.params.id,functions(err,foundrestaurant){
+//
+//   })
+// })
+
+app.post("/restaurants/:id/comments",isLoggedIn, function(req, res) {
+  // var newComment = req.body.comment;
+  // Restaurant.create(newComment,function(err,newlycreated){
+  //   if(err){
+  //     console.log(err)
+  //   }
+  //   else{
+  //     console.log(newlycreated);
+  //     res.redirect("newcomments");
+  //
+  //   }
+  Restaurant.findById(req.params.id, function(err, restaurant) {
+    if (err) {
+      console.log("=+==========")
+      console.log(restaurant)
+      console.log(err);
+    } else {
+      console.log(req.body.comment.text)
+      Comment.create(req.body.comment, function(err, comment) {
+        if (err) {
           console.log(err)
+        } else {
+          // console.log(restaurant)
+          restaurant.comments.push(comment);
+          restaurant.save();
+          res.redirect("/restaurants/" + restaurant._id);
         }
-        else{
-          res.render("newcomment",{restaurant:foundrestaurant});
-        }
-
       })
 
+    }
+  })
+})
+//
+// })
+
+
+//AUTH ROUTES
+
+app.get("/register", function(req, res) {
+  res.render("register");
+})
+
+
+app.post("/register", function(req, res) {
+  var newUser = new User({
+    username: req.body.username
+  });
+  // var password = req.body.password;
+
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err)
+      console.log("==========")
+      return res.render('register')
+    }
+    passport.authenticate("local")(req, res, function() {
+      res.redirect("/login")
     })
-    // app.get("/restaurants/:id/comments/new",function(req,res){
-    //   Restaurant.findById(req.params.id,functions(err,foundrestaurant){
-    //
-    //   })
-    // })
+  })
+  //
+  // console.log(username)
+  // console.log(password)
+  // res.redirect("register")
+})
 
-    app.post("/restaurants/:id/comments",function(req,res){
-      // var newComment = req.body.comment;
-      // Restaurant.create(newComment,function(err,newlycreated){
-      //   if(err){
-      //     console.log(err)
-      //   }
-      //   else{
-      //     console.log(newlycreated);
-      //     res.redirect("newcomments");
-      //
-      //   }
-      Restaurant.findById(req.params.id,function(err,restaurant){
-        if(err){
-          console.log("=+==========")
-          console.log(restaurant)
-          console.log(err);
-        }
-        else{
-          console.log(req.body.comment.text)
-          Comment.create(req.body.comment,function(err,comment){
-            if(err){
-              console.log(err)
-            }
-            else{
-              // console.log(restaurant)
-              restaurant.comments.push(comment);
-              restaurant.save();
-              res.redirect("/restaurants/"+restaurant._id);
-            }
-          })
+app.get("/login",function(req,res){
+  res.render("login")
+})
 
-        }
-      })
-      })
-    //
-    // })
+app.post("/login",passport.authenticate("local",{
+  successRedirect:"/restaurants",
+  failureRedirect:"/login"
+}),function(req,res){
+})
+
+
+app.get("/logout",function(req,res){
+  req.logout(); //passport logs the user out deletes all the sessions
+  res.redirect("/login")
+})
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+
+  }
+  res.redirect("/login");
+}
 
 
 
 
-
-
-    app.listen(3000, function() {
-      console.log("Server started on port 3000");
-    });
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
--- seed.js
+++ seed.js
@@ -5,21 +5,21 @@ var Comment = require("./models/comment")
 var data = [{
     name: "paradise",
     image: "https://www.wearegurgaon.com/wp-content/uploads/2017/07/paradise-biryani-gurgaon-1.jpg",
-    description: "nice biryani but has ghee init"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
 
   },
   {
     name: "pista house",
     image: "https://media.weddingz.in/images/2e68d77263df76529509a7fdb029805c/pista-house-banquet-hall-kukatpally-hyderabad.jpg",
-    description: "best haleem in the city"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
   }, {
     name: "shah-gouse",
     image: "https://imgstaticcontent.lbb.in/lbbnew/wp-content/uploads/2018/03/04220518/ShahGhouse.jpg",
-    description: "paya sherwa is dope"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
   }, {
     name: "Ram ki bandi",
     image: "https://pbs.twimg.com/media/CHCk3vTUAAEqz46.jpg",
-    description: "Great dosa"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
   }
 ]
 function seedDB() {
